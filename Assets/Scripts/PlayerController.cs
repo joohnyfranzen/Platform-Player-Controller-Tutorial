44f9b1d2ae1dbf3165ff62297a5c8aeaa5b9f774
Jonathan <joohny_2014@hotmail.com>
2023-04-17 23:20:29 -0300

    feat: character movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float  movementInputDirection;
+    
+    private Rigidbody2D rb;
+
+    public float movementSpeed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+    }
+    private void FixedUpdate() {
+        ApplyMovement();    
+    }
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+    }
+    private void ApplyMovement()
+    {
+        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
     }
 }
