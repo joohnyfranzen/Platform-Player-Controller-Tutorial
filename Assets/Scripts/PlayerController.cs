f588ac978e50809dfe334f11244f1783c5fe7cdf
Jonathan <joohny_2014@hotmail.com>
2023-04-18 14:44:48 -0300

    feat: Jump Animations and functions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -88,6 +88,8 @@ public class PlayerController : MonoBehaviour
     private void UpdateAnimations()
     {
         anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
     }
     private void CheckInput()
     {
